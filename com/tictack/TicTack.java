057f9e71fd4b9851f219d61c1927b5f31927f95f
rehan7594 <rehansdm94@gmail.com>
2021-04-25 10:21:01 -0700

    Added: create board

--- com/tictack/TicTack.java
+++ com/tictack/TicTack.java
@@ -1,7 +1,18 @@
 package com.tictack;
 
 public class TicTack {
+
+	public static char[] createBoard()
+	{
+		char [] board = new char[10];
+		for(int pos=1;pos<10;pos++)
+		{
+			board[pos] = ' ';
+		}
+		return board; 
+	}
+
 	public static void main(String[] args) {
-		System.out.println("Welcome to Tic Tack Game");
+		char[] board = createBoard();
 	}
 }
